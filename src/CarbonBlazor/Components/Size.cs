4c81de50eb97a33c73929fc4cbb8c531f8703914
TwoChisel <twochisel@gmail.com>
2021-04-20 00:11:33 +0800

    feat(Size): Add Size types

---
+++ src/CarbonBlazor/Components/Size.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel;
+
+namespace CarbonBlazor
+{
+
+    public enum Size
+    {
+        [Description("")]
+        Primary,
+
+        [Description("field")]
+        Field,
+
+        [Description("sm")]
+        Sm,
+
+        [Description("md")]
+        Md,
+
+        [Description("lg")]
+        Lg,
+
+        [Description("xl")]
+        Xl,
+    }
+
+}
